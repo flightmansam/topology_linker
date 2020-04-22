990f3173748ff75820f09d52fc82ed1ec8d474db
Samuel Hutchinson <samuel.hutchinson@mirerigation.com.au>
2020-04-22 18:35:21 +1000

    tabbita

--- topology_linker/projects/water_balance_tabbita.py
+++ topology_linker/projects/water_balance_tabbita.py
@@ -6,9 +6,11 @@ from topology_linker.src.utils import subtract_one_month
 
 period_start = pd.datetime(year=2020, month=1, day=16, hour=00)
 period_end = pd.datetime(year=2020, month=2, day=17, hour=00)
-tysons = water_balance("TYSONS", '26165', '26215', "../out/TYSONS_LINKED.csv", period_start, period_end, use_regs=False, debug=True, show=True, out=['26215'])
+length = 925+514.41+747.72
+tysons = water_balance("TYSONS", '26165', '26215', "../out/TYSONS_LINKED.csv", period_start, period_end,area = length*8, use_regs=False, debug=True, show=True, out=['26215'])
 
-tabbita = water_balance("TABBITA-1", '26215', '26228', "../out/TYSONS_LINKED.csv", period_start, period_end, use_regs=False, debug=True, show=True, out=['26228'])
+length=2200
+tabbita = water_balance("TABBITA-1", '26215', '26228', "../out/TYSONS_LINKED.csv", period_start, period_end,area = length*8, use_regs=False, debug=True, show=True, out=['26228'])
 
 print()
 
